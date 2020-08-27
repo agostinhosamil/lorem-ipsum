ee7bbd35b140a3ce3eabe3f679b6e1a54956c2b8
Samil <53584688+agostinhosamil@users.noreply.github.com>
2020-08-27 06:24:20 -0700

    Create index.php

---
+++ exts/content/index.php
@@ -0,0 +1,16 @@
+<?php
+
+return preg_split ( '/\s+/' ,
+  'Lorem ipsum dolor sit amet consectetur ' .
+  'adipisicing elit sed do eiusmod tempor ' .
+  'incididunt ut labore et dolore magna ali' .
+  'qua Ut enim ad minim veniam quis nostr' .
+  'ud exercitation ullamco laboris nisi ut ' .
+  'aliquip ex ea commodo consequat Duis au' .
+  'te irure dolor in reprehenderit in volup' .
+  'tate velit esse cillum dolore eu fugiat ' .
+  'nulla pariatur Excepteur sint occaecat ' .
+  'cupidatat non proident sunt in culpa qu' .
+  'i officia deserunt mollit anim id est la' .
+  'borum'
+);
